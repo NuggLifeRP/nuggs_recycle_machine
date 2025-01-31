b25c262f581d95aa5c269111b75a836b7a0c30a0
NuggLifeRP <56065414+NuggLifeRP@users.noreply.github.com>
2025-01-31 17:09:13 -0600

    Add files via upload

---
+++ recycle_machines/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'NuggAssassin'
+description 'Recycle Machine Locations'
+version '2.0.0'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/recycle_machine.ytyp'
+
+
